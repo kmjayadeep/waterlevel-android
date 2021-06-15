48f6901194960453d75ccd97b7e86fef6ed4e8a9
Jayadeep KM <kmjayadeep@gmail.com>
2021-06-15 13:45:21 +0530

    Set percentage

--- app/src/main/java/online/cloud16/waterlevelindicator/MainActivity.kt
+++ app/src/main/java/online/cloud16/waterlevelindicator/MainActivity.kt
@@ -1,8 +1,13 @@
 package online.cloud16.waterlevelindicator
 
+import android.graphics.Color
+import android.os.Build
 import android.os.Bundle
 import android.util.Log
+import android.view.*
+import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.ColorRes
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
@@ -16,10 +21,20 @@ class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     private var TAG = "water";
 
+    private lateinit var progressView: View
+    private lateinit var progressText: TextView
+    private lateinit var progressLayout: View
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+        progressView = findViewById(R.id.progress_view)
+        progressLayout = findViewById(R.id.progress_layout)
+        progressText = findViewById(R.id.progress_text)
     }
 
     private fun load() {
@@ -28,12 +43,29 @@ class MainActivity : AppCompatActivity() {
             override fun onDataChange(snapshot: DataSnapshot) {
                 val value = snapshot.value
                 Log.i("val", "Value is: $value")
+                setPercentage(90)
             }
 
             override fun onCancelled(error: DatabaseError) {
                 Log.i("val", "cancelled")
             }
         });
+    }
+
+    private fun setPercentage(n : Int) {
+        progressText.text = "$n%"
+        var maxHeight = progressLayout.height
+        var height = n * maxHeight / 100
+        var layoutParams = progressView.layoutParams
+        layoutParams.height = height
+        progressView.layoutParams = layoutParams
+        if(n<=25) {
+            progressView.setBackgroundColor(Color.parseColor("#ff3333"))
+        }else if (n<=75) {
+            progressView.setBackgroundColor(Color.parseColor("#0080ff"))
+        }else {
+            progressView.setBackgroundColor(Color.parseColor("#4d4dff"))
+        }
     }
 
     public override fun onStart() {
